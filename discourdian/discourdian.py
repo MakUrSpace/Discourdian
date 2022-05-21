a7a347f3f9f6b8e557060155c453ae0b337cec25
musingsole <musingsole@gmail.com>
2022-05-20 23:07:33 -0400

    Image adjustment for Instagram. Filetype filtering for all channels

--- discourdian/discourdian.py
+++ discourdian/discourdian.py
@@ -2,7 +2,8 @@ import discord
 import tweepy
 import praw
 from instagrapi import Client as InstaClient
-
+from random import random
+from PIL import Image, ImageOps, ImageColor
 
 import logging
 from dataclasses import dataclass
@@ -20,6 +21,15 @@ class Content:
     imageUrls: [str]
     caption: str
 
+    @staticmethod
+    def fileTypeCheck(filename, types=None):
+        fileType = filename.split(".")[-1]
+        types = types if types is not None else ['jpg', 'png']
+        return fileType in types
+
+    def allFilesTypeCheck(self, types=None):
+        return sum([self.fileTypeCheck(url, types) for url in self.imageUrls]) == len(self.imageUrls)
+
 
 def postToReddit(content):
     reddit = praw.Reddit(
@@ -30,7 +40,13 @@ def postToReddit(content):
         username="musengdir",
     )
     subreddit = reddit.subreddit("MakUrSpace")
-    subreddit.submit_image(content.caption, content.imageUrls[0])
+    contentUrl = None
+    for url in content.imageUrls:
+        if content.fileTypeCheck(url, ['jpg', 'png', 'gif']):
+            contentUrl = url
+            break
+    if contentUrl is not None:
+        subreddit.submit_image(content.caption, contentUrl)
 
 
 def postToTwitter(content):
@@ -38,16 +54,27 @@ def postToTwitter(content):
         keys.apiKey, keys.apiSecret,
         keys.accessToken, keys.accessSecret
     ))
-    media_ids = [api.media_upload(imageUrl).media_id for imageUrl in content.imageUrls]
-    return api.update_status(status=content.caption, media_ids=media_ids)
+    selectedUrls = [url for url in content.imageUrls if Content.fileTypeCheck(url, ['png', 'jpg'])]
+    media_ids = [api.media_upload(imageUrl).media_id for imageUrl in selectedUrls]
+    if media_ids:
+        return api.update_status(status=content.caption, media_ids=media_ids)
 
 
 def postToInstagram(content):
     ic = InstaClient()
     ic.login("makurspacellc", keys.instaPw)
     media_ids = []
-    for imageUrl in content.imageUrls:
-        media_ids.append(ic.photo_upload(imageUrl, content.caption))
+    for imageUrl in content.imageUrls[:1]:
+        if not content.fileTypeCheck(imageUrl, ['jpg', 'png']):
+            continue
+        jpgPath = f".{''.join(imageUrl.split('.')[:-1])}.jpg"
+        image = Image.open(imageUrl).convert("RGB")
+        width, height = image.size
+        newLength = max(width, height) + 200
+        color = ImageColor.getrgb("orange" if random() > 0.49 else "gray")
+        alteredImage = ImageOps.pad(image, (newLength, newLength), color=color)
+        alteredImage.save(jpgPath)
+        media_ids.append(ic.photo_upload(jpgPath, content.caption))
     return media_ids
 
 
@@ -59,23 +86,25 @@ class Discourdian(discord.Client):
         urls = []
         for attachment in message.attachments:
             filename = attachment.filename
-            if filename.split('.')[-1] == 'jpg':
+            if filename.split('.')[-1] in ['jpg', 'png', 'gif']:
                 path = f"./discourdian_{attachment.filename}"
-                await attachment.save(f"./newMessage_{attachment.filename}")
+                await attachment.save(path)
                 urls.append(path)
         return urls
 
     async def post_content(self, message):
         urls = await self.retrieve_attachments(message)
         content = Content(imageUrls=urls, caption=message.content)
+        print(f"Posting content: {content}...")
         postToReddit(content)
         postToTwitter(content)
         postToInstagram(content)
+        print("Content posted.")
 
     async def on_raw_reaction_add(self, payload):
         channel = self.get_channel(payload.channel_id)
         message = await channel.fetch_message(payload.message_id)
-        if len(message.attachments) < 1:
+        if message.channel.name != "socialplatformcontent" or len(message.attachments) < 1:
             return
 
         # Look for post signal from FinniusDeLaBoomBoom - gear emoji
@@ -89,8 +118,10 @@ class Discourdian(discord.Client):
                 break
 
         if message_approved:
+            print("Approved Message Found! Posting...")
             await self.post_content(message)
 
 
-client = Discourdian()
-client.run(keys.discordKey)
+if __name__ == "__main__":
+    client = Discourdian()
+    client.run(keys.discordKey)
